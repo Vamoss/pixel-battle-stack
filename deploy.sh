9d715ed31c273954473863ce488b7b6ec151f58e
Cristian Mello <cristianc.mello@gmail.com>
2018-08-26 15:59:24 -0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ cd /home/ec2-user
 
 case $DEPLOYMENT_GROUP_NAME in
     "RethinkDB-Prod")
-        APP_ENV=prod docker-compose up -d rethinkdb
+        APP_ENV=prod docker-compose -f docker-compose.prod.yml up -d rethinkdb
         ;;
 esac
