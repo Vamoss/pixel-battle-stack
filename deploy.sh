de0cc1cf0ed0de14da6f6ed67cd32df08603b642
Cristian Mello <cristianc.mello@gmail.com>
2018-08-26 19:48:55 -0300

    Add App Prod group

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,7 @@ case $DEPLOYMENT_GROUP_NAME in
     "RethinkDB-Prod")
         APP_ENV=prod docker-compose -f docker-compose.prod.yml up -d rethinkdb
         ;;
+    "App-Prod")
+        APP_ENV=prod docker-compose -f docker-compose.prod.yml up -d app
+        ;;
 esac
