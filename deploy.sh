c3a3ac45a41e3f0aaf97796eaa87257d56313052
Cristian Mello <cristianc.mello@gmail.com>
2018-08-27 21:36:39 -0300

    Add docker_cleanup script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+superviz_docker_cleanup()
+{
+    echo "Removing all docker objects"
+    {
+        docker kill $(docker ps -q)
+        docker rm $(docker ps -a -q)
+        docker volume rm $(docker volume ls -qf dangling=true)
+        docker volume ls -qf dangling=true | xargs -r docker volume rm
+        docker rmi $(docker images -q -f dangling=true)
+        docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+        docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
+        docker rmi $(docker images -q)
+        docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+    } &> /dev/null
+    echo "Docker was cleaned!"
+    echo
+}
+
+docker_cleanup
+
 cd /home/ec2-user
 
 case $DEPLOYMENT_GROUP_NAME in
