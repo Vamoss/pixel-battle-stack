fb407624c48fa775cfdba734ff1cb6a93f9e2d25
Cristian Mello <cristianc.mello@gmail.com>
2018-08-28 00:04:58 -0300

    Testing...

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ docker_cleanup()
     {
         docker kill $(docker ps -q)
         docker rm $(docker ps -a -q)
-        docker volume rm $(docker volume ls -qf dangling=true)
-        docker volume ls -qf dangling=true | xargs -r docker volume rm
+        #docker volume rm $(docker volume ls -qf dangling=true)
+        #docker volume ls -qf dangling=true | xargs -r docker volume rm
         docker rmi $(docker images -q -f dangling=true)
         docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
         docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
