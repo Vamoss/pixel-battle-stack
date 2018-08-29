a38e39c13a4bcf338a72b9c847e24a4c09257299
Cristian Mello <cristianc.mello@gmail.com>
2018-08-28 22:45:38 -0300

    Enable HTTPS

--- deploy.sh
+++ deploy.sh
@@ -22,10 +22,10 @@ cd /home/ec2-user
 
 case $DEPLOYMENT_GROUP_NAME in
     "RethinkDB-Prod")
-        APP_ENV=prod docker-compose -f docker-compose.db.prod.yml up --build --force-recreate -d rethinkdb
+        docker-compose -f docker-compose.db.prod.yml up --build --force-recreate -d rethinkdb
         ;;
     "App-Prod")
         cp /env/.env app/.env
-        APP_ENV=prod docker-compose -f docker-compose.app.prod.yml up --build --force-recreate -d app
+        docker-compose -f docker-compose.app.prod.yml up --build --force-recreate -d app
         ;;
 esac
